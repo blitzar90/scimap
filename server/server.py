f242f2c5b2850805b60531247824b3fa8a230841
blitzar90 <blitzar90@gmail.com>
2017-07-01 16:14:41 +0300

    logging

--- server/server.py
+++ server/server.py
@@ -1,6 +1,11 @@
+import sys
+sys.path.append('/scimap/')
+from main import logger
 import SocketServer
 import SimpleHTTPServer
 
+logger=logger('./server_log.txt')
+logger.log('Starting server')
 handler=SimpleHTTPServer.SimpleHTTPRequestHandler
 http_Server=SocketServer.TCPServer(('',80),handler)
 http_Server.serve_forever()
