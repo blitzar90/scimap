17c8ee4f532709328a6ed3382dfeec56d4a22133
blitzar90 <blitzar90@gmail.com>
2017-07-12 18:46:23 +0300

    nice commit

--- scimap/urls.py
+++ scimap/urls.py
@@ -5,13 +5,10 @@ from django.conf.urls import url
 from .views import index, admin, nodes, node, routes, route 
 
 urlpatterns = [
-	# url(r'^$', views.index, name='index page'),
-	# url(r'^admin/', views.admin, name='admin page'),
 	url(r'^$', index, name='index page'),
 	url(r'^node/(?P<uuid>[^/]+)/$', node, name='node'),
 	url(r'^route/(?P<uuid>[^/]+)/$', route, name='route'),
 	url(r'^nodes/', nodes, name='nodes'),
 	url(r'^routes/', routes, name='routes'),
 	url(r'^cabinet/', admin, name='admin page'),
-	#url(r'^route/(?P<uuid>[^/]+)/$', route, name='route'),
 ]
